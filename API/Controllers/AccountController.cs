b856242f4f9edf2a8e2c488dcbd906d0e663d8d6
Roman Salo <saloroman23@gmail.com>
2024-04-18 17:30:02 +0300

    Implement user registration

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]  // POST: api/account/register
+        public async Task<ActionResult<UserDto>> Register ([FromBody] RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username))
+            {
+                return BadRequest("This username is taken");
+            }
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser()
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user),
+            };
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == username.ToLower());
+            return user != null;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -18,12 +18,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.UseCors(builder => builder
     .AllowAnyHeader()
     .AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
