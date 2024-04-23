99a7542b4a6c06d8d95a030080673c5d71ffee29
Roman Salo <saloroman23@gmail.com>
2024-04-23 17:41:11 +0300

    Client: add registration form component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -27,4 +27,8 @@ export class HomeComponent implements OnInit {
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
 }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(public accountService: AccountService) { }
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.cancel();
+      },
+      error: error => console.log(error),
+      complete: () => console.log(this.model.username + " registered")
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+}
