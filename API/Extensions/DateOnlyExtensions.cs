309d797931fa802127d2c88615e201dc7654c712
Roman Salo <saloroman23@gmail.com>
2024-05-01 16:11:42 +0300

    API: extend AppUser entity

---
+++ API/Extensions/DateOnlyExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace API.Extensions
+{
+    public static class DateOnlyExtensions
+    {
+        public static int CalculateAge(this DateOnly dateOfBirth)
+        {
+            DateTime now = DateTime.UtcNow;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month)
+            {
+                age--;
+            }
+            else if (now.Month == dateOfBirth.Month)
+            {
+                if (now.Day < dateOfBirth.Day)
+                {
+                    age--;
+                }
+            }
+            return age;
+        }
+    }
+}
