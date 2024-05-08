1874ea7a04693d5e7621237d76ad65b31471d9b1
Roman Salo <saloroman23@gmail.com>
2024-05-08 14:41:19 +0300

    Client: make member-list component to display list of members

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../../_models/member';
+import { MembersService } from '../../_services/members.service';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.css']
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  members: Member[] = [];
 
+  constructor(private membersService: MembersService) { }
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+    loadMembers() {
+      this.membersService.getMembers().subscribe({
+        next: response => this.members = response,
+        error: error => console.log(error),
+        complete: () => console.log("Request completed")
+      });
+    }
 }
