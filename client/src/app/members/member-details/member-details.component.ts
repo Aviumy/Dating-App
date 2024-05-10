070948aa4aedcf3943e994ba1671a4a5fe28eefb
Roman Salo <saloroman23@gmail.com>
2024-05-10 15:48:20 +0300

    Client: make member-details component standalone to use standalone gallery in it

--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,11 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Member } from '../../_models/member';
 import { MembersService } from '../../_services/members.service';
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-member-details',
   templateUrl: './member-details.component.html',
-  styleUrls: ['./member-details.component.css']
+  styleUrls: ['./member-details.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule, TabsModule, 
+  ],
 })
 export class MemberDetailsComponent implements OnInit {
   @Input() username: string = '';
