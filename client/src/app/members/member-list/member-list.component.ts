aad7b1291bccc775b776ecc0336411361d4477be
Roman Salo <saloroman23@gmail.com>
2024-05-14 16:43:17 +0300

    Client: replace member array with observable in member-list component

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../../_models/member';
 import { MembersService } from '../../_services/members.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-member-list',
@@ -8,19 +9,11 @@ import { MembersService } from '../../_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private membersService: MembersService) { }
 
   ngOnInit(): void {
-    this.loadMembers();
+    this.members$ = this.membersService.getMembers();
   }
-
-    loadMembers() {
-      this.membersService.getMembers().subscribe({
-        next: response => this.members = response,
-        error: error => console.log(error),
-        complete: () => console.log("Request completed")
-      });
-    }
 }
