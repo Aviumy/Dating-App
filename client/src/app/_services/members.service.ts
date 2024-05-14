8cc09a71852802341a6edfe9011af6f696a0f523
Roman Salo <saloroman23@gmail.com>
2024-05-14 16:41:57 +0300

    Client: store member list in service to reduce amount of API calls

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,20 +3,34 @@ import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Member } from '../_models/member';
 import { User } from '../_models/user';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0)
+      return of(this.members);
+
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.userName === username);
+    if (member)
+      return of(member);
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
