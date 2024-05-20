1e0d4ae5ce95871893c8310252de029cf86f9a28
Roman Salo <saloroman23@gmail.com>
2024-05-20 17:12:01 +0300

    API: add uploading a photo in UsersController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Interfaces;
 using API.Interfaces.Repositories;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -13,11 +14,13 @@ namespace API.Controllers
     {
         private readonly IUserRepository _repo;
         private readonly IMapper _mapper;
+        private readonly IPhotoService _photoService;
 
-        public UsersController(IUserRepository repo, IMapper mapper)
+        public UsersController(IUserRepository repo, IMapper mapper, IPhotoService photoService)
         {
             _repo = repo;
             _mapper = mapper;
+            _photoService = photoService;
         }
 
         [HttpGet]  // api/users
@@ -48,6 +51,34 @@ namespace API.Controllers
                 return NoContent();
             else
                 return BadRequest("Failed to update user.");
+        }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await _repo.GetByUsernameAsync(User.GetUsername());
+            if (user == null)
+                return NotFound();
+
+            var result = await _photoService.AddPhotoAsync(file);
+            if (result.Error != null)
+                return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+            if (!user.Photos.Any())
+                photo.IsMain = true;
+            user.Photos.Add(photo);
+
+            if (await _repo.SaveAllAsync())
+            {
+                var photoDto = _mapper.Map<PhotoDto>(photo);
+                return CreatedAtAction(nameof(GetUser), new { username = user.UserName }, photoDto);
+            }
+            return BadRequest("Failed to upload photo");
         }
     }
 }
