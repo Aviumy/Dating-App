b7b4e235af7dc75f6b8968cdd228ee29471b0786
Roman Salo <saloroman23@gmail.com>
2024-05-20 17:08:21 +0300

    API: implement uploading a photo

--- API/Extensions/AppServiceExtensions.cs
+++ API/Extensions/AppServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using API.Data;
 using API.Data.Repositories;
+using API.Helpers;
 using API.Interfaces;
 using API.Interfaces.Repositories;
 using API.Services;
@@ -22,6 +23,8 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<IPhotoService, PhotoService>();
 
             return services;
         }
